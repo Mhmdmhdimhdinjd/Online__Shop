1bd85478eba048599d5ce09bd90741853f6d5538
mhmd mhdi <mhdimhdin5@gmail.com>
2025-03-01 15:12:57 +0330

    change file address

--- src/pages/productdetail/productdetailcontainer.jsx
+++ src/pages/productdetail/productdetailcontainer.jsx
@@ -1,61 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { addItem } from '/src/redux/reducers/CartReducer';
-// import Productdetailcomponents from '../../components/productdetails/podectdetailcomponent';
-// import { Paper, Box, Typography } from '@mui/material';
-// import Navbar from '../../Components/Navbar/Index';
-// import Footer from '../../Components/Footer/index';
-// import ProductDetail from '../../ReactQuery/ProductDetail';
-
-// const ProductDetailscontainer = () => {
-//     const { id } = useParams();
-//     const [product, setProduct] = useState(null);
-//     const dispatch = useDispatch();
-//     const {data , isLoading} =ProductDetail(id)
-
-//     useEffect(() => {
-//         setProduct(data)
-//     }, [id]);
-
-//     const addToCart = (product) => {
-
-//         dispatch(addItem(product))
-//     };
-
-//     if (!product) {
-//         return <div>Loading...</div>;
-//     }
-
-//     return (
-
-//         <>
-
-//             <Navbar />
-
-//             <Paper elevation={3} sx={{ borderRadius: '30px' , my:4 }}>
-//                 <Box p={4} color="gray">
-//                     <Typography>{product.category}</Typography>
-//                 </Box>
-
-//                 <Productdetailcomponents addToCart={addToCart} product={product} />
-
-//             </Paper>
-
-//             <Footer />
-
-//         </>
-
-//     )
-// }
-
-// export default ProductDetailscontainer
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addItem } from '/src/redux/reducers/CartReducer';
-import Productdetailcomponents from '../../components/productdetails/podectdetailcomponent';
+import Productdetailcomponents from '../../Components/productdetails/podectdetailcomponent';
 import { Paper, Box, Typography } from '@mui/material';
 import Navbar from '../../Components/Navbar/Index';
 import Footer from '../../Components/Footer/index';
