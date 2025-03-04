53e51dd864092403eb7be7d58c9ee88959a6d6b3
mhmd mhdi <mhdimhdin5@gmail.com>
2025-03-05 01:10:39 +0330

    add dialog to clear cart

--- src/pages/Shoppingbasket/Shoppingbasketcotaner.jsx
+++ src/pages/Shoppingbasket/Shoppingbasketcotaner.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import ProductGrid from "../../Components/shoppigbasket/shoppingbasketgrid/shoppingbasketgrid";
 import Navbar from "../../Components/Navbar/Index";
 import Footer from "../../Components/Footer/index";
-import { Box, Container, Button , Typography } from "@mui/material";
+import { Box, Container, Button, Typography, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 import { clearCart } from "../../redux/reducers/CartReducer";
 import { useNavigate } from "react-router-dom";
 
@@ -17,6 +17,15 @@ const Shoppingbasketcontainer = () => {
 
     const cartItems = useSelector((state) => state.cart.items);
 
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return (
         <>
@@ -40,27 +49,27 @@ const Shoppingbasketcontainer = () => {
 
                     {cartItems.length !== 0 ?
 
-<>
-                        <Button
-                        variant="contained"
-                        color="error"
-                        onClick={() => dispatch(clearCart())}
-                        sx={{ fontFamily: 'gandom' }}
-                    >
-                        خالی کردن سبد خرید
-                    </Button>
+                        <>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                onClick={handleClickOpen}
+                                sx={{ fontFamily: 'gandom' }}
+                            >
+                                خالی کردن سبد خرید
+                            </Button>
 
 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        // onClick={() => dispatch()}
-                        sx={{ fontFamily: 'gandom' }}
-                    >
-                        تکمیل خرید
-                    </Button>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                // onClick={() => dispatch()}
+                                sx={{ fontFamily: 'gandom' }}
+                            >
+                                تکمیل خرید
+                            </Button>
 
-                    </>
+                        </>
                         :
 
                         <Typography
@@ -83,6 +92,35 @@ const Shoppingbasketcontainer = () => {
             </Container>
 
             <Footer />
+
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    <Typography mt={1} fontFamily={'gandom'}>آیا از حذف محصولات از سبد خرید اطمینان دارید؟</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {' !این کار غیر قابل بازگشت است'}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        <Typography fontFamily={'gandom'}>
+                            لغو
+                        </Typography>
+                    </Button>
+                    <Button onClick={() => {
+                        dispatch(clearCart())
+                        handleClose()
+                    }}>
+                        <Typography fontFamily={'gandom'}>حذف</Typography>
+                    </Button>
+                </DialogActions>
+            </Dialog >
 
         </>
     )
