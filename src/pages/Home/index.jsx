7c559c83c43533ea6476932d242c947ac8b804d9
mhmd mhdi <mhdimhdin5@gmail.com>
2025-03-05 16:40:42 +0330

    change address of files

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,7 @@ import NavBar from "../../Components/Navbar/Index";
 import Footer from "../../Components/Footer";
 import Slider from "../../Components/Slider/Slider";
 import Intro from "../../Components/Intro/IntroContainer";
-import ProductShowcase from "../../Components/productsList/ProductShowcase";
+import ProductShowcase from "../../Components/productslist/ProductShowcase";
 
 const Home = () => {
 
